9bd30b90d989cb8cf9a6036df2d521006c8848b8
kakkou0104 <kakkou0104@gmail.com>
2017-08-23 19:08:33 +0900

    ScriptTestプロジェクト　コミット一回目
    array配列を昇順と降順で表示するプログラム

---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+		//array配列を宣言
+		int[] array = { 10, 20, 30, 40, 50 };
+
+		//配列の各要素を順番に表示
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log (array [i]);
+		}
+
+		Debug.Log ("ここから逆順表示");
+
+
+		//逆順に表示
+		for(int i = array.Length-1; i>=0; i--){
+			Debug.Log(array[i]);
+		}
+
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
