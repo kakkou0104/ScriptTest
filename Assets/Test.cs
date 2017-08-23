0a88322f154b769596588043713f97308ea13c1f
kakkou0104 <kakkou0104@gmail.com>
2017-08-23 20:58:17 +0900

    ScriptTest コミット2回目
    Bossクラスにmagic関数を作成して呼び出し

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,27 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss {
+
+	private int mp = 53;//mp
+
+	//magic関数
+	public void magic (){
+
+		this.mp -= 5;//mpを減らす
+
+		if (mp > 0) {
+			Debug.Log ("魔法攻撃をした。残りMPは" + this.mp);
+
+		} else {
+			Debug.Log ("MPが足りないため魔法が使えない。");
+		}
+
+	}
+}
 
 public class Test : MonoBehaviour {
 
 	void Start () {
-		//array配列を宣言
-		int[] array = { 10, 20, 30, 40, 50 };
+		//Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss();
 
-		//配列の各要素を順番に表示
-		for (int i = 0; i < array.Length; i++) {
-			Debug.Log (array [i]);
+		//magic関数を呼び出す
+		for (int i = 0; i < 11; i++) {
+			lastboss.magic ();
 		}
-
-		Debug.Log ("ここから逆順表示");
-
-
-		//逆順に表示
-		for(int i = array.Length-1; i>=0; i--){
-			Debug.Log(array[i]);
-		}
-
-
 	}
 		
 	// Update is called once per frame
